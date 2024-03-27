55bf271f519a81a3da218bee8e0b302efe047cbf
DeyvissonRobert <robert.designerx@gmail.com>
2024-03-26 21:37:06 -0300

    horario de funcionamento ok

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 20; 
+    return hora >= 18 && hora < 22; 
     //true = restaurante está aberto
 }
 
